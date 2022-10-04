e1101d80dee6f54880c06e78b57080a5ba9941a7
shevchenko-nikita <nicksevchenkos@gmail.com>
2022-10-04 23:00:12 +0300

    define structure

--- src/types/transaction.rs
+++ src/types/transaction.rs
@@ -1,13 +1,28 @@
 use crate::types::{AccountId, Balance, Timestamp};
 
+#[derive(Debug)]
 pub struct Transaction {
     nonce: u128,
     timestamp: Timestamp,
-    from: AccountId,
+    from: Option<AccountId>,
     data: TransactionData,
     signature: Option<String>
 }
 
+impl Transaction {
+    pub fn new(data : TransactionData, from : Option<AccountId>) -> Self {
+        Transaction{
+            nonce: 0,
+            timestamp: 0,
+            data,
+            from,
+            signature: None
+
+        }
+    }
+}
+
+#[derive(Debug)]
 pub enum TransactionData {
     CreateAccount(AccountId),
     MintInitialSupply{to:AccountId, amount: Balance},
